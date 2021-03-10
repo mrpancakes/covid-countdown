9f6978f303855352616abf9d8335dcf9054966e7
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-03-10 13:45:13 -0600

    Added countdown.js files and made the timer tick with setInterval.

--- assets/main.js
+++ assets/main.js
@@ -3,10 +3,17 @@ let partyDay = moment("2021-04-16")
 
 $("#todaysDate").text(`${today.format('MMMM D, YYYY')}`);
 
+// function daysRemaining(){
+//     $("#partyDay").text(`${partyDay.diff(today, "days", "")} Days Remaining`);
+// }
 
 
-function daysRemaining(){
-    $("#partyDay").text(`${partyDay.diff(today, "days")} Days Remaining`);
-}
+// daysRemaining();
 
-daysRemaining()
+
+
+setInterval(function(){
+    
+    $("#partyDay").text(moment("2021-04-17").countdown().toString());
+
+}, 1000)
